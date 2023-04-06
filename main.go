4aae023c0b3302fec19c58089c955ca6aeb3ae88
triet <pvmtriet232@gmail.com>
2023-04-06 20:59:20 +0700

    add constructor

--- main.go
+++ main.go
@@ -3,15 +3,16 @@ package main
 import "fmt"
 
 func main() {
-	var triet = Saiyan{
+	var triet = &Saiyan{
 		Name:  "Triet",
 		Power: 9000,
 	}
-	var ptr = &triet
-	ptr.Power = 5000
-
-	fmt.Println(triet)
-	fmt.Println(ptr)
+	triet.Hello()
+	fmt.Println(triet.Power)
+	// fmt.Printf("triet power currently is: %v\n", triet.Power)
+	// fmt.Println(&triet.Power, *&triet.Power)
+	khoa := NewSaiyan("Khoa", 9003)
+	fmt.Println(khoa)
 }
 
 type Saiyan struct {
@@ -21,4 +22,13 @@ type Saiyan struct {
 
 func (s *Saiyan) Hello() {
 	s.Power = s.Power + 10000
+	// fmt.Printf("power inside a function %v\n", s.Power)
+	// fmt.Println(&s.Power, *&s.Power)
+}
+
+func NewSaiyan(name string, power int) Saiyan {
+	return Saiyan{
+		Name:  name,
+		Power: power,
+	}
 }
