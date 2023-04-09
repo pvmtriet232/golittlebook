d2cd0b3be19cb6eff07d0ec88d9d442998a538f8
triet <pvmtriet232@gmail.com>
2023-04-09 16:57:53 +0700

    add

--- main.go
+++ main.go
@@ -7,10 +7,20 @@ import (
 )
 
 func main() {
-	scores := []int{1, 2, 3, 4, 5}
-	slice := scores[2:4]
-	slice[0] = 999
-	fmt.Println(scores)
+	Vegeta := &models.Saiyan{Name: "Vegeta", Power: 9000, Father: &models.Saiyan{Name: "King Vegeta", Power: 15000}}
+	Goku := &models.Saiyan{Name: "Goku", Power: 10000, Father: &models.Saiyan{Name: "Bardock", Power: 8000}}
+	Broly := &models.Saiyan{Name: "Broly", Power: 50000, Father: &models.Saiyan{Name: "Paragus", Power: 3000}}
+	Gohan := &models.Saiyan{Name: "Gohan", Power: 8000, Father: nil}
+	warriors := []*models.Saiyan{Vegeta, Goku, Gohan, Broly}
+
+	Cell := NewSaiyan("Cell", 9000, nil)
+	warriors = append(warriors, &Cell)
+	fmt.Println(warriors[1].Name)
+	for index, _ := range warriors {
+
+		fmt.Printf("name: %v\n", warriors[index].Name)
+	}
+
 }
 
 func NewSaiyan(name string, power int, father *models.Saiyan) models.Saiyan {
