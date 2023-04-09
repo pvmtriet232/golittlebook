b62fc62f3064b0ee7dfbf8f9bf83f7037b845d7b
triet <pvmtriet232@gmail.com>
2023-04-09 15:43:46 +0700

    add

--- main.go
+++ main.go
@@ -2,15 +2,38 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/pvmtriet232/golittlebook/models"
 )
 
 func main() {
+	Vegeta := &models.Saiyan{Name: "Vegeta", Power: 9000, Father: &models.Saiyan{Name: "King Vegeta", Power: 15000}}
+	Goku := &models.Saiyan{Name: "Goku", Power: 10000, Father: &models.Saiyan{Name: "Bardock", Power: 8000}}
+	Broly := &models.Saiyan{Name: "Broly", Power: 50000, Father: &models.Saiyan{Name: "Paragus", Power: 3000}}
+	Gohan := &models.Saiyan{Name: "Gohan", Power: 8000, Father: nil}
+	warriors := []*models.Saiyan{Vegeta, Goku, Gohan, Broly}
+	fmt.Println(extractPowers(warriors))
 
-	scores := []int{2, 5, 2, 6, 67, 7, 1245, 234}
-	for index, num := range scores {
-		fmt.Printf("index : %v, number : %v\n", index, num)
+	for index, _ := range warriors {
+
+		fmt.Printf("name: %v\n", warriors[index].Name)
+	}
+	Cell := NewSaiyan("Cell", 9000, nil)
+	fmt.Println(Cell)
+}
+func NewSaiyan(name string, power int, father *models.Saiyan) models.Saiyan {
+	return models.Saiyan{
+		Name:   name,
+		Power:  power,
+		Father: nil,
 	}
 
 }
 
-// declare a struct
+func extractPowers(s []*models.Saiyan) []int {
+	powers := make([]int, len(s))
+	for index, value := range s {
+		powers[index] = value.Power
+	}
+	return powers
+}
