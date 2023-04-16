17fc735248b3a68fdf9fd0e155d1fb3a1a3120c3
triet <pvmtriet232@gmail.com>
2023-04-17 00:15:19 +0700

    map

--- maps/main.go
+++ maps/main.go
@@ -3,8 +3,30 @@ package main
 import "fmt"
 
 func main() {
-	lookup := make(map[string]int)
-	lookup["goku"] = 9001
-	power, exists := lookup["vegeta"]
-	fmt.Println(power, exists)
+	saiyanMap := make(map[string]int)
+	saiyanMap["goku"] = 9001
+	saiyanMap["vegeta"] = 19000
+	saiyanMap["gohan"] = 5000
+	fmt.Println(saiyanMap)
+	for key, value := range saiyanMap {
+		fmt.Printf("Saiyan name: %v , power: %v\n", key, value)
+	}
+
+	saiyanMapTwo := map[string]int{
+		"goku":   9001,
+		"vegeta": 19000,
+	}
+	fmt.Println(saiyanMapTwo)
+
+	type saiyan struct {
+		Name string
+	}
+	goku := &saiyan{"goku"}
+	vegeta := &saiyan{"vegeta"}
+	a := make([]saiyan, 2)
+	b := make([]*saiyan, 2)
+	fmt.Println(a, b, goku, vegeta)
+	b = append(b, goku)
+	fmt.Println(a, b, goku, vegeta)
+
 }
