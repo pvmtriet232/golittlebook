4754c2281f31f557a696db2b485dcbc6645b94d5
triet <pvmtriet232@gmail.com>
2023-04-17 14:46:51 +0700

    add a SaiyanList

--- main.go
+++ main.go
@@ -1,5 +1,28 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/pvmtriet232/golittlebook/db"
+)
+
 func main() {
+	var SaiyanList = make([]*db.Saiyan, 10)
+
+	SaiyanList[0] = &db.Saiyan{
+		Name:  "goku",
+		Power: 9001,
+	}
+	SaiyanList[1] = &db.Saiyan{
+		Name:  "Vegeta",
+		Power: 19000,
+	}
+	SaiyanList[2] = &db.Saiyan{
+		Name:  "Nappa",
+		Power: 12002,
+	}
+
+	A := &SaiyanList[1].Power
+	fmt.Println(A)
 
 }
